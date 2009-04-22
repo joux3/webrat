d3c15471a549586ec89388eacf7343bedb1c5a22
joux3 <joku_rules@hotmail.com>
2009-04-22 22:09:17 +0300

    merged with the webrat master, fixed wang support

--- lib/webrat/wang.rb
+++ lib/webrat/wang.rb
@@ -1,3 +1,35 @@
-require "wang"
+require 'wang'
 
-require File.dirname(__FILE__) + "/wang/wang_session"
+module Webrat
+  WangResponse = Struct.new(:body, :session)
+  class WangSession < Session
+    def get(url, data, h = {})
+      data = data.map {|k,v| "#{WANG::Utils.escape(k)}=#{WANG::Utils.escape(v)}"}.join("&") if data.is_a?(Hash)
+      if data
+        url += url.include?('?') ? "&#{data}" : "?#{data}"
+      end
+      # wang doesn't allow passing custom headers, only passing referers
+      @response_status, @response_headers, @response_body = wang.get(url, h['HTTP_REFERER'])
+    end
+
+	def response
+		WangResponse.new(response_body, self)
+	end
+
+    def post(url, data)
+      @response_status, @response_headers, @response_body = wang.post(url, data)
+    end
+
+    def response_body
+      @response_body
+    end
+
+    def response_code
+      @response_status
+    end
+      
+	def wang 
+	  @wang ||= WANG.new
+	end
+  end
+end
--- wang_spike.rb
+++ wang_spike.rb
@@ -4,7 +4,7 @@ require "lib/webrat/wang"
 include Webrat
 
 sess = WangSession.new
-sess.visits "http://www.google.com/"
-sess.fills_in "q", :with => "Webrat"
-sess.clicks_button
+sess.visit "http://www.google.com/"
+sess.fill_in "q", :with => "Webrat"
+sess.click_button
 sess.save_and_open_page
